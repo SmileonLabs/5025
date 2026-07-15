85cef77a347440714d5929379a8d2e25ab22720c
ljhee3611-cmyk <262798815+ljhee3611-cmyk@users.noreply.github.com>
2026-07-15 15:28:27 +0900

    Apply Great Question notebook migration on deploy

--- scripts/post-merge.sh
+++ scripts/post-merge.sh
@@ -5,4 +5,5 @@ psql "$DATABASE_URL" -v ON_ERROR_STOP=1 \
   -f lib/db/migrations/0001_reading_conversation.sql \
   -f lib/db/migrations/0002_book_mission_link.sql \
   -f lib/db/migrations/0003_weekly_missions.sql \
-  -f lib/db/migrations/0004_great_questions.sql
+  -f lib/db/migrations/0004_great_questions.sql \
+  -f lib/db/migrations/0005_great_question_notebook.sql
